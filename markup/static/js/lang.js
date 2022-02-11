9d3dfafab059ce2bc5bbf80df697c87d79d111c2
User <uriyderechey@gmail.com>
2022-02-11 15:51:49 +0200

    fx txt

---
+++ markup/static/js/lang.js
@@ -0,0 +1,220 @@
+const langArr = {
+    "about-title" : {
+        "ru" : "О НАС",
+        "en" : "ABOUT US"
+
+    },
+
+    "about-text" : {
+        "ru" : "Мы - школа для нового поколения дизайнеров с нашим стилем и видением. Наши преподаватели оценят ваши смелые решения и помогут развить профессиональное чутье для реализации в самой быстрорастущей сфере. Присоединяйтесь к нам!",
+        "en" : "We are a school for a new generation of designers with our style and vision. Our teachers will appreciate your courageous decisions and help develop a professional flair for implementation in the fastest-growing field. Join us!"
+    },
+
+    "btn" : {
+        "ru" : "Подробнее",
+        "en" : "More information"
+    },
+
+    "design" : {
+        "ru" : "UX/UI Дизайн",
+        "en" : "UX/UI Design"
+    },
+
+    "design-price" : {
+        "ru" : "<strong>Стартовый пакет от 190 евро</strong>",
+        "en" : "<strong>Starter pack from 190 euro</strong>"
+    },
+
+    "design-text" : {
+        "ru" : "Научитесь создавать прототипы мобильных приложений, разрабатывать дизайн веб-сайтов, рисовать баннеры, логотипы и другие элементы фирменного стиля. Станьте специалистом среднего звена быстрее, чем это можно сделать в любой другой области.",
+        "en" : "Learn how to create mobile app prototypes, design websites, draw banners, logos, and other corporate identity elements. Become a middle-level specialist faster than any other field might offer."
+    },
+
+    "3ddesign" : {
+        "ru" : "3D Дизайн",
+        "en" : "3D Desgin"
+    },
+
+    "3ddesign-text" : {
+        "ru" : "Создавать реалистичные изображения и применять принципы пространственной и цветовой композиции, используя базовые знания программ 3dsmax, AutoCAD/Autodesk, Maya/Maxon и Cinema4D.",
+        "en" : "Create realistic images and apply the principles of spatial and color composition, using basic knowledge of 3dsmax, AutoCAD/Autodesk, Maya/Maxon, and Cinema4D programs."
+    },
+
+    "3ddesign-price" : {
+        "ru" : "<strong>Стартовый пакет от 190 евро</strong>",
+        "en" : "<strong>Starter pack from 190 euro</strong>"
+    },
+
+    "photoshop" : {
+        "ru" : "Photoshop и Illustrator",
+        "en" : "Photoshop and Illustrator"
+    },
+
+    "photoshop-text" : {
+        "ru" : 'Получите некоторые знания и используйте классический редактор векторной графики Illustrator для создания логотипов, макетов и комбинируйте его с продуктами Adobe "s Creative Cloud, включая InDesign и Photoshop.',
+        "en" : 'Gain some knowledge and use classic vector graphics editor Illustrator for creating logos, layout, and combine it with Adobe"s Creative Cloud products, including InDesign and Photoshop.'
+    },
+
+    "photoshop-price" : {
+        "ru" : "<strong>Стартовый пакет от 190 евро</strong>",
+        "en" : "<strong>Starter pack from 190 euro</strong>"
+    },
+
+    "typography" : {
+        "ru" : "Логотип и типографика",
+        "en" : "Logo and Typography"
+    },
+
+    "typography-text" : {
+        "ru" : 'Получите некоторые знания и используйте классический редактор векторной графики Illustrator для создания логотипов, макетов и комбинируйте его с продуктами Adobe "s Creative Cloud, включая InDesign и Photoshop.',
+        "en" : 'Gain some knowledge and use classic vector graphics editor Illustrator for creating logos, layout, and combine it with Adobe"s Creative Cloud products, including InDesign and Photoshop.'
+    },
+
+    "typography-price" : {
+        "ru" : "<strong>Стартовый пакет от 190 евро</strong>",
+        "en" : "<strong>Starter pack from 190 euro</strong>"
+    },
+
+
+    "smart" : {
+        "ru" : 'Умное творчество',
+        "en" : 'Smart creativity'
+    },
+
+    "alt" : {
+        "ru" : 'Стоимость всех курсов согласовывается в индивидуальном порядке, в зависимости от необходимого объема для изучения. </br> Для уточнения стоимости необходимо заполнить контактную форму.',
+        "en" : 'The cost of all courses is agreed on an individual basis, depending on the required volume for study. </br> To clarify the cost, you should fill out a contact form.'
+    },
+
+    "smart-text" : {
+        "ru" : 'Наши студенты учатся сочетать практические навыки и творческое мышление в идеальных пропорциях, что позволяет им действовать продуманно и ярко, создавая портфолио.',
+        "en" : 'Our students learn to combine practical skills and creative thinking in ideal proportions, which allows them to act thoughtfully and brightly, creating a portfolio.'
+    },
+
+    "individual" : {
+        "ru" : 'Индивидуальный подход',
+        "en" : 'Individual approach'
+    },
+
+    "individual-text" : {
+        "ru" : 'Перед началом обучения мы хотим знать все о вкусовых предпочтениях наших студентов и их ожиданиях. Мы также учитываем уровень подготовки студентов, чтобы максимально раскрыть их способности.',
+        "en" : 'Before we start studying, we want to know everything about the taste preferences of our students and their expectations. We also take into account students" backgrounds to maximize their abilities.'
+    },
+
+    "attention" : {
+        "ru" : 'Внимание к деталям',
+        "en" : 'Attention to detail'
+    },
+
+    "attention-text" : {
+        "ru" : 'Мы знаем, что мелочи важны, поэтому мы работаем с каждой деталью и учим этому. Кроме того, мы создаем уникальный авторский стиль, который делает портфолио наших студентов неповторимыми.',
+        "en" : 'We know that the little things are important, so we work with every detail and teach it. In addition, we create a unique authoring style that makes the portfolio of our students unique.'
+    },
+
+    "contacts-title" : {
+        "ru" : "Напишите нам",
+        "en" : "Write to us"
+    },
+
+    "contacts-title2" : {
+        "ru" : "Напишите нам",
+        "en" : "Write to us"
+    },
+
+    "contacts-title3" : {
+        "ru" : "Свяжитесь с нами",
+        "en" : "Contact us"
+    },
+
+    "contacts-text" : {
+        "ru" : "Не терпится присоединиться к нашему творческому окружению? Не медлите! Оставьте свой запрос, и наш менеджер свяжется с вами в ближайшее время.",
+        "en" : "Can't wait to join our creative surroundings? Don't hesitate! Leave your inquiry, and our manager will contact you soon."
+    },
+
+
+    "contacts-name" : {
+        "ru" : "Имя",
+        "en" : "Name"
+    },
+
+    "contacts-phone" : {
+        "ru" : "Телефон",
+        "en" : "Phone"
+    },
+
+    "contacts-email" : {
+        "ru" : "Почта",
+        "en" : "Email"
+    },
+
+    "contacts-btn" : {
+        "ru" : "Отправить",
+        "en" : "Send"
+    },
+
+    "contacts-name2" : {
+        "ru" : "Имя",
+        "en" : "Name"
+    },
+
+    "contacts-phone2" : {
+        "ru" : "Телефон",
+        "en" : "Phone"
+    },
+
+    "contacts-email2" : {
+        "ru" : "Почта",
+        "en" : "Email"
+    },
+
+    "contacts-btn2" : {
+        "ru" : "Отправить",
+        "en" : "Send"
+    },
+
+    "home" : {
+        "ru" : "Главная",
+        "en" : "Home"
+    },
+
+    "about" : {
+        "ru" : "О нас",
+        "en" : "About"
+    },
+
+    "classes" : {
+        "ru" : "Занятия",
+        "en" : "Classes"
+    },
+
+    "gui" : {
+        "ru" : "Руководство",
+        "en" : "Guidance"
+    },
+
+    "values" : {
+        "ru" : "Значения",
+        "en" : "Values"
+    },
+
+    "contacts" : {
+        "ru" : "Контакты",
+        "en" : "Contacts"
+    },
+
+    "form-subtitle" : {
+        "ru" : "Не терпится присоединиться к нашему творческому окружению? Не медлите! Оставьте свой запрос, и наш менеджер свяжется с вами в ближайшее время.",
+        "en" : "Can't wait to join our creative surroundings? Don't hesitate! Leave your inquiry, and our manager will contact you soon."
+    },
+
+    "privacy" : {
+        "ru" : "Политика конфиденциальности",
+        "en" : "Privacy Policy"
+    },
+
+    "terms" : {
+        "ru" : "Условия и положения",
+        "en" : "Terms & Conditions"
+    }
+
+}
