53e4544e2403566910a33afe0a5e92fe8ecec7b5
uriyderechey <iurii.derechei@aspira.global>
2024-11-04 19:45:09 +0200

    fx content

---
+++ builds_all/build_S_1_theme/static/js/custom.js
@@ -0,0 +1,167 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var height = $(window).scrollTop();
+
+        if (height > 50) {
+            $('.js-header').addClass('is-scroll');
+        } else {
+            $('.js-header').removeClass('is-scroll');
+        }
+    });
+    $('.header__nav-link').bind('click', function (e) {
+        $('.nav-bar').removeClass('toggle');
+        $('.header__line').removeClass('toggle');
+    });
+    $('.js-slider').on('init.slick', function () {
+        $(this).removeClass('is-fix-height');
+    }).slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        cssEase: 'linear'
+    });
+    $('#scroll').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+    $('.popup-btn').click(function () {
+        $('.popup').addClass('open');
+    });
+    $('.js-close').click(function () {
+        $('.popup').removeClass('open');
+    });
+});
+var toggleButton = document.querySelector('.header__toggle-menu');
+var toggleLine1 = document.querySelector('.line1');
+var toggleLine2 = document.querySelector('.line2');
+var toggleLine3 = document.querySelector('.line3');
+var navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', function () {
+    navBar.classList.toggle('toggle');
+    toggleLine1.classList.toggle('toggle');
+    toggleLine2.classList.toggle('toggle');
+    toggleLine3.classList.toggle('toggle');
+});
+var msg = document.querySelector('.msg');
+var gsapMsg = gsap.to('.msg', 0.25, {
+    autoAlpha: 1,
+    y: -40,
+    ease: Expo.inOut,
+    paused: true
+});
+var arrInput = document.querySelectorAll('.aInput');
+
+function send(event, php) {
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    for (var i = 0, count = arrInput.length; i < count; i++) {
+        arrInput[i].classList.remove('inputerror');
+    }
+
+    event.target.querySelector('button').disabled = true;
+    showMsg('Wait. Sending...', '#b1b1b1');
+    var req = new XMLHttpRequest();
+    req.open('POST', php, true);
+
+    req.onload = function () {
+        event.target.querySelector('button').disabled = false;
+
+        if (req.status >= 200 && req.status < 400) {
+            var json = JSON.parse(this.response);
+
+            if (json.result === 'success') {
+                showMsg('Message send', '#36AE46', '1000');
+                setTimeout(function () {
+                    document.getElementsByClassName('msg')[0].style.opacity = '0';
+                }, 2000);
+                event.target.reset();
+            } else if (json.result === 'email') {
+                showMsg('Error. Need email', '#DC352F');
+                setTimeout(function () {
+                    document.getElementsByClassName('msg')[0].style.opacity = '0';
+                }, 2000);
+                document.querySelector('#email').classList.add('inputerror');
+            }
+        } else {
+            showMsg('Server error. number: ' + req.status, '#DC352F');
+            setTimeout(function () {
+                document.getElementsByClassName('msg')[0].style.opacity = '0';
+            }, 2000);
+        }
+    };
+
+    req.onerror = function () {
+        showMsg('Error sending request', '#DC352F');
+    };
+
+    req.send(new FormData(event.target));
+}
+
+function showMsg(message, color) {
+    msg.innerText = message;
+    msg.style.background = color;
+    gsapMsg.restart();
+}
+
+function inputFile(e) {
+    var el = e.target.parentNode.querySelector('.count');
+    if (e.target.value !== '') el.innerHTML = 'Selected files: ' + e.target.files.length;else el.innerHTML = 'Select file';
+}
+
+for (var i = 0, count = arrInput.length; i < count; i++) {
+    arrInput[i].addEventListener('focus', function () {
+        this.nextElementSibling.classList.add('active');
+    });
+    arrInput[i].addEventListener('blur', function () {
+        if (this.value === false) {
+            this.nextElementSibling.classList.remove('active');
+        }
+    });
+}
+
+window.onload = function () {
+    var loadPage = gsap.timeline();
+    loadPage.to('#form', 0.7, {
+        autoAlpha: 1,
+        y: 0,
+        ease: Expo.inOut
+    });
+
+};
+
+const select = document.querySelector('select');
+const allLang = ['en', 'ua'];
+
+select.addEventListener('change', changeURLLanguage);
+// перенаправить url c указанием языка
+function changeURLLanguage () {
+    let lang = select.value
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function  changeLanguage () {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    if (!allLang.includes(hash) ) {
+        location.href = window.location.pathname + '#en';
+        location.reload()
+    }
+    select.value = hash;
+
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-'+key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+    }
+
+}
+
+changeLanguage();
+
---
+++ builds_all/build_S_1_theme/static/js/lang.js
@@ -0,0 +1,179 @@
+const langArr = {
+    "about-title" : {
+        "ua" : "ПРО НАС",
+        "en" : "ABOUT US"
+
+    },
+
+    "about-text" : {
+        "ua" : "Вітаємо на сайті нашої студії! Ми віримо, що дизайн – це не лише про естетику, а й про створення функціонального простору, що надихає та підкреслює вашу індивідуальність",
+        "en" : "Welcome to the website of our studio! We believe that design is not only about aesthetics, but also about creating a functional space that inspires and emphasizes your personality"
+    },
+
+    "btn" : {
+        "ua" : "Більше",
+        "en" : "More information"
+    },
+
+    "design" : {
+        "ua" : "UX/UI Дизайн",
+        "en" : "UX/UI Design"
+    },
+
+    "design-text" : {
+        "ua" : "UX/UI дизайн – це не просто гарна картинка. Це основа зручності та задоволення користувача від взаємодії з продуктом. Ми створюємо інтерфейси, які ведуть користувача до цілі, роблячи цей шлях інтуїтивним та приємним.",
+        "en" : "“UX/UI design is not just a pretty picture. It is the basis of user convenience and satisfaction from interaction with the product. We create interfaces that lead the user to the goal, making this path intuitive and enjoyable."
+    },
+
+    "3ddesign" : {
+        "ua" : "3D Дизайн",
+        "en" : "3D Desgin"
+    },
+
+    "3ddesign-text" : {
+        "ua" : "3D дизайн – це можливість оживити ваші ідеї ще до їхньої реалізації. Завдяки тривимірним моделям ви можете побачити продукт у всій його деталізації, оцінити пропорції, кольори та текстури. Ми створюємо віртуальні світи, які надихають та вражають своєю реалістичністю.",
+        "en" : "3D design is an opportunity to bring your ideas to life before they are realized. Thanks to three-dimensional models, you can see the product in all its detail, evaluate its proportions, colors, and textures. We create virtual worlds that inspire and impress with their realism."
+    },
+
+    "photoshop" : {
+        "ua" : "Photoshop та Illustrator",
+        "en" : "Photoshop and Illustrator"
+    },
+
+    "photoshop-text" : {
+        "ua" : 'Photoshop та Illustrator – це інструменти, що дають нам змогу перетворювати ваші ідеї у візуально вражаючі проєкти. Photoshop забезпечує детальне редагування зображень та створення фотореалістичних композицій, тоді як Illustrator ідеальний для точних і чистих векторних ілюстрацій.',
+        "en" : 'Photoshop and Illustrator are the tools that enable us to turn your ideas into visually stunning projects. Photoshop provides detailed image editing and photorealistic compositing, while Illustrator is ideal for precise, clean vector illustrations.'
+    },
+
+    "typography" : {
+        "ua" : "Логотип та Типографіка",
+        "en" : "Logo and Typography"
+    },
+
+    "typography-text" : {
+        "ua" : 'Логотип і типографіка – це обличчя вашого бренду. Вони створюють перше враження, передають стиль та цінності компанії, допомагаючи їй виділятися серед конкурентів. Ми віримо, що кожна деталь має значення – від форми логотипу до шрифту, який ви обираєте.',
+        "en" : 'A logo and typography are the face of your brand. They create the first impression, convey the style and values of the company, helping it stand out from the competition. We believe that every detail matters - from the shape of the logo to the font you choose.'
+    },
+
+    "smart" : {
+        "ua" : 'Розумна Творчість',
+        "en" : 'Smart Creativity'
+    },
+
+    "smart-text" : {
+        "ua" : 'Розумна Творчість – це поєднання креативності, яка спирається на аналіз та бізнес-стратегію.</br> Ми впроваджуємо інноваційні ідеї, що не тільки приваблюють аудиторію, але й допомагають досягти конкретних результатів для вашого бренду.',
+        "en" : 'Smart Creativity is a combination of creativity based on analysis and business strategy.</br> We implement innovative ideas that not only attract the audience, but also help to achieve specific results for your brand.',
+    },
+
+    "individual" : {
+        "ua" : 'Індивідуальний Підхід',
+        "en" : 'Individual Approach'
+    },
+
+    "individual-text" : {
+        "ua" : 'Індивідуальний підхід – це основа нашої роботи. Ми віримо, що кожен проєкт має бути унікальним, відображати цінності вашого бренду та відповідати вашим конкретним цілям. Наше завдання – зрозуміти вашу ідею та втілити її в дизайн, що перевершує очікування.',
+        "en" : 'Individualized approach is the basis of our work. We believe that every project should be unique, reflect your brand values, and meet your specific goals. Our task is to understand your idea and translate it into a design that exceeds expectations.'
+    },
+
+    "attention" : {
+        "ua" : 'Увага до Деталей',
+        "en" : 'Attention to details'
+    },
+
+    "attention-text" : {
+        "ua" : 'Увага до деталей – це принцип, на якому будується вся наша робота. Ми віримо, що кожна дрібниця має значення, адже саме від деталей залежить, чи виглядатиме проєкт професійно, гармонійно та завершено.',
+        "en" : 'Attention to detail is the principle on which all our work is based. We believe that every little thing matters, because it is the details that determine whether a project will look professional, harmonious, and complete.'
+    },
+
+    "contacts-title" : {
+        "ua" : "Напишіть нам",
+        "en" : "Write to us"
+    },
+
+    "contacts-title2" : {
+        "ua" : "Напишіть нам",
+        "en" : "Write to us"
+    },
+
+    "contacts-title3" : {
+        "ua" : "Зв'яжіться с нами",
+        "en" : "Contact us"
+    },
+
+    "contacts-text" : {
+        "ua" : "Маєте ідею чи питання? Ми завжди раді новим проєктам та можливостям співпраці. Зв’яжіться з нами, щоб обговорити ваші ідеї або дізнатися більше про наші послуги!",
+        "en" : "Do you have an idea or a question? We are always looking for new projects and opportunities for cooperation. Contact us to discuss your ideas or learn more about our services!"
+    },
+
+
+    "contacts-name" : {
+        "ua" : "Ім'я",
+        "en" : "Name"
+    },
+
+    "contacts-phone" : {
+        "ua" : "Телефон",
+        "en" : "Phone"
+    },
+
+    "contacts-email" : {
+        "ua" : "Пошта",
+        "en" : "Email"
+    },
+
+    "contacts-btn" : {
+        "ua" : "Надіслати",
+        "en" : "Send"
+    },
+
+    "contacts-name2" : {
+        "ua" : "Ім'я",
+        "en" : "Name"
+    },
+
+    "contacts-phone2" : {
+        "ua" : "Телефон",
+        "en" : "Phone"
+    },
+
+    "contacts-email2" : {
+        "ua" : "Пошта",
+        "en" : "Email"
+    },
+
+    "contacts-btn2" : {
+        "ua" : "Надіслати",
+        "en" : "Send"
+    },
+
+    "home" : {
+        "ua" : "Головна",
+        "en" : "Home"
+    },
+
+    "about" : {
+        "ua" : "Про нас",
+        "en" : "About"
+    },
+
+    "classes" : {
+        "ua" : "Заняття",
+        "en" : "Classes"
+    },
+
+    "gui" : {
+        "ua" : "Керівництво",
+        "en" : "Guidance"
+    },
+
+    "values" : {
+        "ua" : "Значення",
+        "en" : "Values"
+    },
+
+    "contacts" : {
+        "ua" : "Контакти",
+        "en" : "Contacts"
+    },
+
+}
